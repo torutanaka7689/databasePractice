631c2176c4b105084aab3e89f1188c4f41769a2f
toru tanaka <t.tanaka.cingroup2012@gmail.com>
2025-02-05 12:39:07 +0900

    構文の修正

--- pg_practice.sql
+++ pg_practice.sql
@@ -1,19 +1,19 @@
 -- 問題1. postgresqlでpracticeデータベースを生成するSQL文を記述してください。
 CREATE DATABASE practice;
 -- 問題2. postgresqlでpracticeデータベースのusersテーブルを生成するSQL文を記述してください。
-# CREATE TABLE users
-# (id SERIAL NOT NULL,
-# name char(255) NOT NULL DEFAULT '' Comment '氏名'
-# age int NOT NULL DEFAULT 0 
-# gender enum('Man','Woman','Other') NOT NULL DEFAULT 'Other' Comment '性別',
-# PRIMARY KEY(id));
+CREATE TABLE users
+(id SERIAL NOT NULL,
+name char(255) NOT NULL DEFAULT COMMENT ON COLUMN users.name IS '氏名',
+age integer NOT NULL DEFAULT 0,
+gender enum('Man','Woman','Other') NOT NULL DEFAULT 'Other' COMMENT ON COLUMN users.gender IS '性別',
+PRIMARY KEY(id));
 
 -- 問題3. postgresqlでpracticeデータベースのjobsテーブルを生成するSQL文を記述してください。
-# CREATE TABLE jobs
-# (id SERIAL NOT NULL,
-# user_id int NOT NULL,
-# name char(255) NOT NULL DEFAULT '会社員' Comment '仕事名',
-# gender enum('Man','Woman','Other') NOT NULL DEFAULT 'Other' Comment '性別',
-# PRIMARY KEY(id)
-# FOREIGN KEY (user_id) REFERENCES users(id)
+CREATE TABLE jobs
+(id SERIAL NOT NULL,
+user_id integer NOT NULL,
+name char(255) NOT NULL DEFAULT '会社員' COMMENT ON COLUMN jobs.name IS '仕事名',
+gender enum ('Man','Woman','Other') NOT NULL DEFAULT COMMENT ON COLUMN jobs.gender IS '性別',
+PRIMARY KEY(id),
+FOREIGN KEY (user_id) REFERENCES users(id)
 );
