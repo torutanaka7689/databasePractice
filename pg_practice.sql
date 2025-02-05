ae568eaad25e972f600ee3e81597c0c94efcf219
toru tanaka <t.tanaka.cingroup2012@gmail.com>
2025-02-05 16:46:25 +0900

    アドバイスをもとに修正

--- pg_practice.sql
+++ pg_practice.sql
@@ -5,7 +5,7 @@ CREATE TABLE users
 (id SERIAL NOT NULL,
 name char(255) NOT NULL DEFAULT COMMENT ON COLUMN users.name IS '氏名',
 age integer NOT NULL DEFAULT 0,
-gender enum('Man','Woman','Other') NOT NULL DEFAULT 'Other' COMMENT ON COLUMN users.gender IS '性別',
+CREATE TYPE gender As ENUM('Man','Woman','Other') NOT NULL DEFAULT 'Other' COMMENT ON COLUMN users.gender IS '性別',
 PRIMARY KEY(id));
 
 -- 問題3. postgresqlでpracticeデータベースのjobsテーブルを生成するSQL文を記述してください。
@@ -13,7 +13,6 @@ CREATE TABLE jobs
 (id SERIAL NOT NULL,
 user_id integer NOT NULL,
 name char(255) NOT NULL DEFAULT '会社員' COMMENT ON COLUMN jobs.name IS '仕事名',
-gender enum ('Man','Woman','Other') NOT NULL DEFAULT COMMENT ON COLUMN jobs.gender IS '性別',
 PRIMARY KEY(id),
 FOREIGN KEY (user_id) REFERENCES users(id)
 );
